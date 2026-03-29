a5f9f7cdcdac66ad9c834cfb7ce84d0f022bdf47
Daswaget <daswaget@gmail.com>
2026-03-29 21:54:20 +0400

    Version 2

--- scripts/navigator.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    var userLang = navigator.language || navigator.userLanguage;
-
-    if (userLang.startsWith("ru")) {
-        window.location.href = "/ru/index.html";
-    } else {
-        window.location.href = "/en/index.html";
-    }
-});
