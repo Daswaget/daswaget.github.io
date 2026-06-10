c960beff93550d19b29d89aa2bf42f9699668479
Daswaget <daswaget@gmail.com>
2026-06-10 23:01:17 +0400

    Update

--- scripts/load_blocks.js
+++ scripts/load_blocks.js
@@ -1,4 +1,4 @@
-function loadContent(url, elementId) {
+function loadContent(url, elementId, callback) {
     fetch(url)
         .then(response => {
             if (!response.ok) {
@@ -7,11 +7,46 @@ function loadContent(url, elementId) {
             return response.text();
         })
         .then(data => {
-            document.getElementById(elementId).innerHTML = data;
+            const el = document.getElementById(elementId);
+            el.innerHTML = data;
+
+            if (callback) callback();
         })
         .catch(error => console.error('Error:', error));
 }
 document.addEventListener("DOMContentLoaded", function () {
-    loadContent('/blocks/header.html', 'header');
+    loadContent('/blocks/header.html', 'header', initHeaderMenu);
     loadContent('/blocks/footer.html', 'footer');
+    loadContent('/blocks/screen_effect.html', 'effects');
 });
+document.addEventListener("DOMContentLoaded", function () {
+    const btn = document.getElementById("menuButton");
+    const menu = document.getElementById("dropdownMenu");
+
+    if (!btn || !menu) return;
+
+    btn.addEventListener("click", function () {
+        menu.classList.toggle("hidden");
+    });
+
+    document.addEventListener("click", function (e) {
+        if (!menu.contains(e.target) && e.target !== btn) {
+            menu.classList.add("hidden");
+        }
+    });
+});
+function initHeaderMenu() {
+    const btn = document.getElementById("menuButton");
+    const menu = document.getElementById("dropdownMenu");
+
+    if (!btn || !menu) return;
+
+    btn.addEventListener("click", function (e) {
+        e.stopPropagation();
+        menu.classList.toggle("hidden");
+    });
+
+    document.addEventListener("click", function () {
+        menu.classList.add("hidden");
+    });
+}
